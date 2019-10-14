09105161af1c63d4a799e641730f619629455e6e
Rafael Manzano Medina <rafamanzanomedina@gmail.com>
2019-10-14 10:21:38 +0200

    Ejercicio Listas Dinamicas

---
+++ nbaTeams/app/src/main/java/es/iesnervion/rmanzano/nbateams/Adaptadores/Adaptador.java
@@ -0,0 +1,7 @@
+package es.iesnervion.rmanzano.nbateams.Adaptadores;
+
+import android.widget.ArrayAdapter;
+
+public class Adaptador extends ArrayAdapter {
+    Adaptador(Content c, int resourceId, int textId, T[] objects);
+}
---
+++ nbaTeams/app/src/main/java/es/iesnervion/rmanzano/nbateams/MainActivity.java
@@ -0,0 +1,18 @@
+package es.iesnervion.rmanzano.nbateams;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    /*
+    Para el equipo foto, texto y conferencia
+    Jugador dorsal, nombre, nacionalidad
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
