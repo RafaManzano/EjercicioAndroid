a000dc3e35c4c978ae10f473a9d988e68fc179cf
Rafael Manzano Medina <rafamanzanomedina@gmail.com>
2019-11-18 08:19:26 +0100

    Subiendo

--- MesaFutsalCompadre/app/src/main/java/es/iesnervion/rmanzano/mesafutsalcompadre/JuegoActivity.java
+++ MesaFutsalCompadre/app/src/main/java/es/iesnervion/rmanzano/mesafutsalcompadre/JuegoActivity.java
@@ -1,8 +1,15 @@
 package es.iesnervion.rmanzano.mesafutsalcompadre;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.LayoutInflater;
@@ -11,127 +18,219 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Chronometer;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Locale;
 
+import es.iesnervion.rmanzano.mesafutsalcompadre.Fragmentos.DorsalDialog;
 import es.iesnervion.rmanzano.mesafutsalcompadre.ViewModel.PartidoViewModel;
 
 
 public class JuegoActivity extends AppCompatActivity {
-    private static final long START_TIME_IN_MILLIS = 1200000;
-
+    //Para la actividad
     private String equipo1;
     private String equipo2;
-    private Chronometer chrono;
     private TextView nombreEquipo1;
     private TextView nombreEquipo2;
-    private PartidoViewModel viewModel;
     private TextView golLocal;
     private TextView golVisitante;
     private TextView faltaL;
     private TextView faltaV;
-    //private Long tiempoParado = Long.valueOf(0);
-   // private Long diferencia = Long.valueOf(0);
-    private CountDownTimer mCountDownTimer;
-    private TextView mTextViewCountDown;
     private ListView equipoLocalTarjetas;
 
+    //ViewModel
+    private PartidoViewModel viewModel;
+
+    //Cronometro
+    private static final long START_TIME_IN_MILLIS = 1200000;
+    private CountDownTimer mCountDownTimer;
+    private TextView mTextViewCountDown;
     private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
 
-    //private Button locales;
+    //Esto es para los fragmentos
+    private TextView dorsal;
+    private Fila fila;
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Esto se comenta, es una prueba para ver si el listado esta correcto
+        /*
         ArrayList<Fila> filas = new ArrayList<Fila>();
+        fila = new Fila(R.drawable.amarilla, "1");
         filas.add(new Fila(R.drawable.amarilla, "7"));
         filas.add(new Fila(R.drawable.roja, "2"));
+        */
+
+
+        //onCreate de Android
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_juego);
-        viewModel = new PartidoViewModel();
+
+        //FindViewById para lo necesario
         nombreEquipo1 = findViewById(R.id.equipo1);
         nombreEquipo2 = findViewById(R.id.equipo2);
         golLocal = findViewById(R.id.golLocal);
         golVisitante = findViewById(R.id.golVisitante);
         equipoLocalTarjetas = findViewById(R.id.listaLocal);
+        faltaL = findViewById(R.id.numeroFaltaLocal);
+        faltaV = findViewById(R.id.numeroFaltaVisitante);
+
+
+        //Con esto realizamos un Intent Explicito y recogemos los datos de la otra actividad
         Bundle extras = getIntent().getExtras();
-
-        //locales = findViewById(R.id.local);
-        //SystemClock.setCurrentTimeMillis(1200000);
-
         equipo1 = extras.getString("j1");
         equipo2 = extras.getString("j2");
         nombreEquipo1.setText(equipo1);
         nombreEquipo2.setText(equipo2);
-        faltaL = findViewById(R.id.numeroFaltaLocal);
-        faltaV = findViewById(R.id.numeroFaltaVisitante);
-        Adaptador adapter = new Adaptador(filas);
-        equipoLocalTarjetas.setAdapter(adapter);
+
+        //Adaptador para la lista de tarjetas
+        //Adaptador adapter = new Adaptador();
+        //equipoLocalTarjetas.setAdapter(adapter);
+
         //Cronometro
         mTextViewCountDown = findViewById(R.id.mTextViewCountDown);
 
-        //chrono.base
-        //chrono.setBase(SystemClock.elapsedRealtime() + 1200000);
-
-        //chrono.setCountDown(true);
-        //chrono.start();
+        //ViewModel
+        viewModel = new PartidoViewModel();
         viewModel = ViewModelProviders.of(this).get(PartidoViewModel.class);
-        mostrarNumerosViewModel();
 
-        //locales.setOnClickListener(this);
+
+        viewModel.getTarjetasAmarillas().observe(this, new Observer<ArrayList<Fila>>() {
+                    @Override
+                    //Si entra en este metodo quiere decir que hay un cambio para notificar
+                    public void onChanged(ArrayList<Fila> filas) {
+                        //Se usa el adaptador
+                        Adaptador adapter = new Adaptador(filas);
+                        equipoLocalTarjetas.setAdapter(adapter);
+                    }
+                });
+
+
+
+
+        mostrarNumerosViewModel(); //Con este metodo retornamos lo correcto cuando hay un cambio de configuracion
 
 
     }
+    //ESTO ES UN DIALOG FRAGMENT
+    //SE QUE NO DEBE IR AQUI PERO ES COMO FUNCIONA
+    public class DorsalDialog extends DialogFragment {
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
 
+            final AlertDialog.Builder builder = new AlertDialog.Builder(JuegoActivity.this);
+            // Get the layout inflater
+            final LayoutInflater inflater = JuegoActivity.this.getLayoutInflater();
+            final View texto = inflater.inflate(R.layout.alertdialog, null);
+            //builder.
+
+            // Inflate and set the layout for the dialog
+            // Pass null as the parent view because its going in the dialog layout
+            builder.setView(texto)
+
+                    // Add action buttons
+                    .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int id) {
+                            //texto = inflater.inflate(R.layout.alertdialog, null);
+                            dorsal = texto.findViewById(R.id.etDorsal);
+                            fila = new Fila(R.drawable.amarilla,  dorsal.getText().toString());
+                            //Le queda anhadir el viewModel
+                            //ArrayList<Fila> filas = viewModel.getFilas().getValue();
+                            //filas.add(fila);
+                            viewModel.anhadirTarjetaAmarilla(fila);
+                            //viewModel.getFilas().add(fila);
+                        }
+                    })
+                    .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+            return builder.create();
+        }
+    }
+
+    /*
+    Para sumar el gol local con el boton, con ViewModel
+     */
     public void sumaGolLocal(View view) {
         viewModel.setGolLocal(viewModel.getGolLocal() + 1);
         golLocal.setText(viewModel.getGolLocal().toString());
     }
 
+    /*
+    Para sumar el gol visitante con el boton, con ViewModel
+     */
     public void sumaGolVisitante(View view) {
         viewModel.setGolVisitante(viewModel.getGolVisitante() + 1);
         golVisitante.setText(viewModel.getGolVisitante().toString());
     }
 
+    /*
+    Para sumar las faltas locales con el boton, Con ViewModel
+     */
     public void faltaLocal(View view) {
         viewModel.setFaltaLocal(viewModel.getFaltaLocal() + 1);
         faltaL.setText(viewModel.getFaltaLocal().toString());
     }
 
+    /*
+    Para sumar las faltas visitantes con el boton, Con ViewModel
+     */
     public void faltaVisitante(View view) {
         viewModel.setFaltaVisitante(viewModel.getFaltaVisitante() + 1);
         faltaV.setText(viewModel.getFaltaVisitante().toString());
     }
 
+    /*
+    Con este metodo mostramos el dialog para poder elegir un dorsal
+     */
+    public void botonAmarilla(View v) {
+        DorsalDialog dd = new DorsalDialog();
+        dd.onCreateDialog(Bundle.EMPTY).show();
+
+    }
+
+    /*
+    Con este metodo al pulsar en el boton iniciar, empezaria el cronometro
+     */
     public void iniciarCronometro(View view) {
+        //Este seria el contador hacia atras
+        //mTimeLeftInMillis =  20 Min //Esta al principio en el comentario de Cronometro
+        //1000 ms = 1s es lo que va a contar
         mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
                 mTimeLeftInMillis = millisUntilFinished;
                 //viewModel.setChrono(millisUntilFinished);
+                //Con este metodo actualizara el textView cuando pase el tiempo
                 updateCountDownText();
             }
 
+            //No haria falta porque no tenemos ningun problema cuando finalice
             @Override
             public void onFinish() {
 
             }
         }.start();
-        //chrono.setBase(viewModel.getChrono() + diferencia);
-        //chrono.start();
     }
 
+    /*
+    Con este metodo se para el cronometro, es el boton parar
+     */
     public void pararCronometro(View view) {
         mCountDownTimer.cancel();
-        //tiempoParado = SystemClock.elapsedRealtime();
-        //chrono.stop();
-        //viewModel.setChrono(chrono.getBase());
+
     }
 
+    /*
+    Con este metodo actualiza el texto con los segundos pasados
+     */
     private void updateCountDownText() {
         int minutes = (int) (mTimeLeftInMillis / 1000) / 60;
         int seconds = (int) (mTimeLeftInMillis / 1000) % 60;
@@ -141,38 +240,10 @@ public class JuegoActivity extends AppCompatActivity {
         mTextViewCountDown.setText(timeLeftFormatted);
     }
 
+
     /*
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.local:
-                sumaGolLocal(v);
-            break;
-
-            case R.id.visitante:
-                sumaGolVisitante(v);
-            break;
-
-            case R.id.btnFaltasLocal:
-                faltaLocal(v);
-            break;
-
-            case R.id.btnFaltasVisitante:
-                faltaVisitante(v);
-            break;
-
-            case R.id.iniciar:
-                iniciarCronometro(v);
-            break;
-
-            case R.id.parar:
-                pararCronometro(v);
-            break;
-        }
-    }
+        Con este metodo cuando hay un cambio de configuracion, mostraremos los valores guardados en ViewModel
     */
-
-
     public void mostrarNumerosViewModel() {
         golLocal.setText(viewModel.getGolLocal().toString());
         golVisitante.setText(viewModel.getGolVisitante().toString());
@@ -182,7 +253,7 @@ public class JuegoActivity extends AppCompatActivity {
     }
 
     /*
-        Lista heterogenea
+        Es la clase Adaptador para la lista, De momento solo tarjetas amarillas equipo local
      */
 
     public class Adaptador extends BaseAdapter {
@@ -208,17 +279,7 @@ public class JuegoActivity extends AppCompatActivity {
             return position;
         }
 
-        /*
-        @Override
-        public int getItemViewType(int position) {
-            int lugar = 0;
-            if((Fila)getItem(position).getFoto() == R.drawable.amarilla) {
-                lugar = 1;
-            }
-            return lugar;
-        }
-        */
-
+        //GetView con ViewHolder
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             View row = convertView;
@@ -247,6 +308,9 @@ public class JuegoActivity extends AppCompatActivity {
         }
     }
 
+    /*
+    La clase ViewHolder, para poder hacer el reciclado
+     */
     public class ViewHolder {
         ImageView imagen;
         TextView texto;
--- MesaFutsalCompadre/app/src/main/java/es/iesnervion/rmanzano/mesafutsalcompadre/ViewModel/PartidoViewModel.java
+++ MesaFutsalCompadre/app/src/main/java/es/iesnervion/rmanzano/mesafutsalcompadre/ViewModel/PartidoViewModel.java
@@ -2,13 +2,21 @@ package es.iesnervion.rmanzano.mesafutsalcompadre.ViewModel;
 
 import android.os.SystemClock;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import es.iesnervion.rmanzano.mesafutsalcompadre.Fila;
 
 public class PartidoViewModel extends ViewModel {
     private Integer golLocal;
     private Integer golVisitante;
     private Integer faltaLocal;
     private Integer faltaVisitante;
+    ArrayList<Fila> filas = new ArrayList<>();
+    private MutableLiveData<ArrayList<Fila>> tarjetasAmarillas;
     //private Long chrono;
 
     public PartidoViewModel() {
@@ -16,6 +24,9 @@ public class PartidoViewModel extends ViewModel {
         golVisitante = 0;
         faltaLocal = 0;
         faltaVisitante = 0;
+        tarjetasAmarillas = new MutableLiveData<>();
+        //filas = new ArrayList<Fila>();
+        //f0.ilas.add(new Fila())
         //chrono = 1200000L;
     }
 
@@ -51,14 +62,14 @@ public class PartidoViewModel extends ViewModel {
         this.faltaVisitante = faltaVisitante;
     }
 
-    /*
-    public Long getChrono() {
-        return chrono;
+    public MutableLiveData<ArrayList<Fila>> getTarjetasAmarillas() {
+        return tarjetasAmarillas;
     }
 
-    public void setChrono(Long chrono) {
-        this.chrono = chrono;
+    public void anhadirTarjetaAmarilla(Fila fila) {
+        filas.add(fila);
+        tarjetasAmarillas.setValue(filas);
     }
-    */
+
 
 }
