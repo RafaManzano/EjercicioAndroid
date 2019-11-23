c14c1d017a34076c573035ccca725184550144b4
Rafael Manzano Medina <rafamanzanomedina@gmail.com>
2019-11-23 20:43:43 +0100

    Layout
    Todavia tengo que anhadir funcionalidades y que se cierre una vez que se pulse resolver

---
+++ profesorPokemonCompadre/app/src/main/java/es/iesnervion/rmanzano/profesorpokemoncompadre/Fragments/Pokemon.java
@@ -0,0 +1,62 @@
+package es.iesnervion.rmanzano.profesorpokemoncompadre.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import es.iesnervion.rmanzano.profesorpokemoncompadre.R;
+
+public class Pokemon extends Fragment implements  View.OnClickListener {
+    private EditText et;
+    private TextView tv;
+    private Button b;
+    private ImageView iv;
+    public Pokemon() {
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //return super.onCreateView(inflater, container, savedInstanceState);
+        View v = inflater.inflate(R.layout.pokemon, container, false);
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(View v, Bundle savedInstanceState) {
+        b = v.findViewById(R.id.resolver);
+        et = v.findViewById(R.id.escribir);
+        tv = v.findViewById(R.id.pokemon);
+        iv = v.findViewById(R.id.imagen);
+        iv.setImageResource(R.drawable.shinxxx);
+        b.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int punto = 0;
+       String respuesta = et.getText().toString();
+       if(respuesta.equalsIgnoreCase(tv.getText().toString())) {
+           //Aqui guardar en el viewModel
+           punto++;
+       }
+
+       onDestroy();
+
+    }
+}
--- profesorPokemonCompadre/app/src/main/java/es/iesnervion/rmanzano/profesorpokemoncompadre/MainActivity.java
+++ profesorPokemonCompadre/app/src/main/java/es/iesnervion/rmanzano/profesorpokemoncompadre/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+
+import es.iesnervion.rmanzano.profesorpokemoncompadre.Fragments.Pokemon;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void prueba1(View view) {
+        Pokemon pok = new Pokemon();
+        getSupportFragmentManager().beginTransaction().add(R.id.framel, pok).commit();
     }
 }
