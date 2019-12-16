04f0125d9e58a0bdf5dc3a22da6c6c6918cfa51d
Rafael Manzano Medina <rafamanzanomedina@gmail.com>
2019-12-16 10:10:42 +0100

    empezando piedra papel tijera

---
+++ PiedraPapelTijeraCompader/app/src/main/java/es/iesnervion/rmanzano/piedrapapeltijeracompader/MainActivity.java
@@ -0,0 +1,21 @@
+package es.iesnervion.rmanzano.piedrapapeltijeracompader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void jugar(View view) {
+        Intent intent = new Intent(this, JuegoActivity.class);
+        startActivity(intent);
+    }
+}
--- profesorPokemonCompadre/app/src/main/java/es/iesnervion/rmanzano/profesorpokemoncompadre/Fragments/AyudaDialogFragment.java
+++ profesorPokemonCompadre/app/src/main/java/es/iesnervion/rmanzano/profesorpokemoncompadre/Fragments/AyudaDialogFragment.java
@@ -10,7 +10,7 @@ import androidx.fragment.app.DialogFragment;
 public class AyudaDialogFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // Use the Builder class for convenient dialog construction
+
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setMessage("En esta imagen hay 7 curiosidades escondidas, encuéntralas y descubre al pokemon correspondiente para obtener un punto, cuanto más puntos más sabes de pokemon. Adelante y capturalos a todos")
                 .setPositiveButton("Entendido", new DialogInterface.OnClickListener() {
@@ -18,7 +18,7 @@ public class AyudaDialogFragment extends DialogFragment {
                         dialog.cancel();
                     }
                 });
-        // Create the AlertDialog object and return it
+
         return builder.create();
     }
 
