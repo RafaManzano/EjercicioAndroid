2ac569204e8987008aadb18e6f75871cbe3dd91d
Rafael Manzano Medina <rafamanzanomedina@gmail.com>
2019-12-19 12:45:48 +0100

    Queda acabar

---
+++ PiedraPapelTijeraCompader/app/src/main/java/es/iesnervion/rmanzano/piedrapapeltijeracompader/EstadisticasActivity.java
@@ -0,0 +1,15 @@
+package es.iesnervion.rmanzano.piedrapapeltijeracompader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class EstadisticasActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estadisticas);
+    }
+}
