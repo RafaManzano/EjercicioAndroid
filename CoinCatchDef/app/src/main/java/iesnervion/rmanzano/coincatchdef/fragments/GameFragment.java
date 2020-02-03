caeb332631b496e87cb19a915504c7710fb09268
Rafael Manzano Medina <rafamanzanomedina@gmail.com>
2020-02-03 11:08:32 +0100

    Subiendo
    Quedaria revisar errores, introducir las imagenes de sonido para poder quitarlos. Probalemente los meta en el sharedPreferences para que tambien se guarden para siempre

--- CoinCatchDef/app/src/main/java/iesnervion/rmanzano/coincatchdef/fragments/GameFragment.java
+++ CoinCatchDef/app/src/main/java/iesnervion/rmanzano/coincatchdef/fragments/GameFragment.java
@@ -7,6 +7,7 @@ import androidx.appcompat.widget.AppCompatImageButton;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -48,6 +49,8 @@ public class GameFragment extends Fragment implements View.OnClickListener {
     private ArrayList<Item> items = new ArrayList<>();
     private TextView monedasMostrar;
     private MainViewModel mainViewModel;
+    private MediaPlayer mediaplayer;
+    private MediaPlayer soundplayer;
 
 
     public GameFragment() {
@@ -73,6 +76,107 @@ public class GameFragment extends Fragment implements View.OnClickListener {
         items = methods.randomizarLista(methods.listadeItems());
 
         //Los finds de los botones (18)
+        todosfinds(v);
+
+
+        //Para mostrar las monedas que estan capturadas
+        monedasMostrar = v.findViewById(R.id.monedasgame);
+
+        //El onclickListener de cada boton (18)
+        todosclicks();
+
+
+        //mainViewModel
+        mainViewModel = ViewModelProviders.of(getActivity()).get(MainViewModel.class);
+
+        //Musica de fondo
+        mediaplayer = MediaPlayer.create(getActivity(), R.raw.cancionfondo);
+        mediaplayer.setVolume(0.3f, 0.3f); //Con esto le ponemos un volumen mas bajo para que suene mas alto los sonidos
+        mediaplayer.start(); //Iniciamos la musica
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        View view =  v;
+
+        if(v.getId() == R.id.plantarse) {
+            mainViewModel.setBotonPulsado(9); //Enviamos a la actividad la pulsacion del boton "plantarse"
+        }
+        else {
+            //Con la lista de elementos que antes hemos cargado y la clase Item elegimos la imagen para mostrar
+            view.setBackgroundResource(items.get(Integer.parseInt(view.getTag().toString())).getImagen());
+
+
+            switch (items.get(Integer.parseInt(view.getTag().toString())).getImagen()) { //Dependiendo del tag que tiene cada imagen
+
+                case R.drawable.ic_moneda:
+                    mainViewModel.setMonedas(mainViewModel.getMonedas() + 5);
+                    soundplayer = MediaPlayer.create(getActivity(), R.raw.soundmoneda); //Sonido de moneda
+                    soundplayer.start();
+                    break;
+
+                case R.drawable.ic_agujeronegro:
+                    mainViewModel.setMonedas(0); //Ponemos las monedas a 0
+                    mainViewModel.setBotonPulsado(6); //Enviamos a la actividad la pulsacion del boton "agujeroNegro"
+                    break;
+
+                case R.drawable.ic_luna:
+                    soundplayer = MediaPlayer.create(getActivity(), R.raw.soundluna); //Sonido de luna
+                    soundplayer.start();
+                    break;
+
+                case R.drawable.ic_meteorito:
+                    soundplayer = MediaPlayer.create(getActivity(), R.raw.soundmeteorito); //Sonido de meteorito
+                    soundplayer.start();
+                    mainViewModel.setMonedas(mainViewModel.getMonedas() - 3);
+                    break;
+
+                case R.drawable.ic_cohete:
+                    soundplayer = MediaPlayer.create(getActivity(), R.raw.soundnave2); //Sonido de nave
+                    soundplayer.start();
+                    mainViewModel.setMonedas(mainViewModel.getMonedas() * 2);
+                    break;
+
+            }
+
+        }
+
+        //Mostramos en la pantalla las monedas
+        if(mainViewModel.getMonedas() < 0) {
+            mainViewModel.setMonedas(0);
+            monedasMostrar.setText( "" + mainViewModel.getMonedas());
+        }
+        else {
+            monedasMostrar.setText("" + mainViewModel.getMonedas());
+        }
+
+
+    }
+
+    /*
+    Cuando el movil se pausa detenemos la musica
+     */
+    @Override
+    public void onPause() {
+        mediaplayer.pause();
+        super.onPause();
+    }
+
+    /*
+    Cuando el movil vuelve en primer plano, iniciamos de nuevo la musica de fondo
+     */
+    @Override
+    public void onResume() {
+        super.onResume();
+        mediaplayer.start();
+
+    }
+
+    /*
+    Con este metodo tenemos todos los findViewById fuera del onViewCreated
+     */
+    public void todosfinds(View v) {
         uno = v.findViewById(R.id.uno);
         dos = v.findViewById(R.id.dos);
         tres = v.findViewById(R.id.tres);
@@ -92,11 +196,12 @@ public class GameFragment extends Fragment implements View.OnClickListener {
         diecisiete = v.findViewById(R.id.diecisiete);
         dieciocho = v.findViewById(R.id.dieciocho);
         plantarse = v.findViewById(R.id.plantarse);
+    }
 
-        //Para mostrar las monedas que estan capturadas
-        monedasMostrar = v.findViewById(R.id.monedasgame);
-
-        //El onclickListener de cada boton (18)
+    /*
+    Con este metodo tenemos todos los setOnClickListener fuera del onViewCreated
+     */
+    public void todosclicks() {
         uno.setOnClickListener(this);
         dos.setOnClickListener(this);
         tres.setOnClickListener(this);
@@ -116,64 +221,5 @@ public class GameFragment extends Fragment implements View.OnClickListener {
         diecisiete.setOnClickListener(this);
         dieciocho.setOnClickListener(this);
         plantarse.setOnClickListener(this);
-
-
-        //mainViewModel
-        mainViewModel = ViewModelProviders.of(getActivity()).get(MainViewModel.class);
     }
-
-    @Override
-    public void onClick(View v) {
-        View view =  v;
-
-        if(v.getId() == R.id.plantarse) {
-            mainViewModel.setBotonPulsado(9);
-        }
-        else {
-            //Con la lista de elementos que antes hemos cargado y la clase Item elegimos la imagen para mostrar
-            view.setBackgroundResource(items.get(Integer.parseInt(view.getTag().toString())).getImagen());
-
-
-            switch (items.get(Integer.parseInt(view.getTag().toString())).getImagen()) {
-
-                case R.drawable.ic_moneda:
-                    mainViewModel.setMonedas(mainViewModel.getMonedas() + 5);
-                    break;
-
-                case R.drawable.ic_agujeronegro:
-                    mainViewModel.setMonedas(0);
-                    mainViewModel.setBotonPulsado(6);
-                    //getSupportFragmentManager().beginTransaction().replace(R.id.frame, finalizar).commit();
-                    break;
-
-                case R.drawable.ic_luna:
-                    //La luna no hace nada
-                    break;
-
-                case R.drawable.ic_meteorito:
-                    mainViewModel.setMonedas(mainViewModel.getMonedas() - 3);
-                    break;
-
-                case R.drawable.ic_cohete:
-                    mainViewModel.setMonedas(mainViewModel.getMonedas() * 2);
-                    break;
-
-            }
-
-        }
-
-
-
-        if(mainViewModel.getMonedas() < 0) {
-            mainViewModel.setMonedas(0);
-            monedasMostrar.setText( "" + mainViewModel.getMonedas());
-        }
-        else {
-            monedasMostrar.setText("" + mainViewModel.getMonedas());
-        }
-
-
-    }
-
-
 }
